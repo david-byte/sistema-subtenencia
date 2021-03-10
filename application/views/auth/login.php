4f4629542f140015a30218424a6c9b4f6a290f96
David Developer <davidanderson04@live.com>
2021-03-10 14:25:03 -0300

    estilizando tela de login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,28 +1,60 @@
-<h1><?php echo lang('login_heading');?></h1>
-<p><?php echo lang('login_subheading');?></p>
+<!DOCTYPE html>
+<html lang="pt-br">
 
-<div id="infoMessage"><?php echo $message;?></div>
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<?php echo form_open("auth/login");?>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?php echo base_url('public/css/app.css'); ?>">
 
-  <p>
-    <?php echo lang('login_identity_label', 'identity');?>
-    <?php echo form_input($identity);?>
-  </p>
+  <title>Login</title>
+</head>
 
-  <p>
-    <?php echo lang('login_password_label', 'password');?>
-    <?php echo form_input($password);?>
-  </p>
+<body>
+  <div class="container">
+    <div class="row centered justify-content-center">
+      <div class="shadow p-3 g-white rounded">
+        <div class="row justify-content-center">
+          <div class="col-md-6">
+            <?php $attributes = array('class' => 'col-md12', 'id' => 'myform', 'role' => 'form'); ?>
+            <?php echo form_open('auth/login', $attributes); ?>
+            <div class="form-group">
+              <?php echo form_label('Identidade:', '', array('class' => 'mt-3')); ?>
+              <?php echo form_input($identity, '', array('class' => 'form-control', 'autofocus')); ?>
+            </div>
+            <div class="form-group">
+              <?php echo form_label('Senha:'); ?>
+              <?php echo form_input($password, '', array('class' => 'form-control')); ?>
+            </div>
+            <?php echo form_submit('button', 'Entrar', array('type' => 'submit', 'class' => 'btn btn-md btn-primary')); ?>
+            <?php echo form_close(); ?>
+          </div>
+          <div class="col-md-4">
+            <div class="justify-content-md-center col-md-10">
+              <img src="<?php echo base_url('public/img/logo.jpeg'); ?>" class="img-thumbnail" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
-  <p>
-    <?php echo lang('login_remember_label', 'remember');?>
-    <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
-  </p>
+  </div>
 
+  <!-- Optional JavaScript; choose one of the two! -->
 
-  <p><?php echo form_submit('submit', lang('login_submit_btn'));?></p>
+  <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 
-<?php echo form_close();?>
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+  <!--
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    -->
+</body>
 
-<p><a href="forgot_password"><?php echo lang('login_forgot_password');?></a></p>
+</html>
