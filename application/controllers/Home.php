54784fd55292a93af97e5a58655282833c32227b
David Developer <davidanderson04@live.com>
2021-03-10 16:29:44 -0300

    adicionando a chamada das views

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,6 +4,10 @@ class Home extends CI_Controller
 {
     public function index()
     {
+        $this->load->view('layout/header');
+        $this->load->view('layout/sidebar');
+        $this->load->view('layout/navbar');
         $this->load->view('home/index');
+        $this->load->view('layout/footer');
     }
 }
